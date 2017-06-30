8b4158db3e1ea4d200bb8f4ab223a3c5f9314894
John Handelaar <john@handelaar.org>
2017-06-30 20:11:49 +0100

    step 1

--- scraper.php
+++ scraper.php
@@ -2,17 +2,21 @@
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+
+// Read in a page
+$html = scraperwiki::scrape("https://mapalerts.corkcoco.ie/en/alerts");
+
+// Find something on the page using css selectors
+$dom = new simple_html_dom();
+$dom->load($html);
+$items = $dom->find("table th a[style='color: #590f56 !important;']");
+print_r($items);
+
+
+
+
 // // Write out to the sqlite database using scraperwiki library
 // scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
 //
