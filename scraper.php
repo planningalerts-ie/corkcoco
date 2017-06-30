30280317c6cc8c50155f87e1f1fc6643ae914591
John Handelaar <john@handelaar.org>
2017-06-30 20:36:22 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -31,6 +31,7 @@ foreach ($targets as $target) {
 function getKML($html) {
 	$tempA = explode('www.mapalerter.ie\/maie\/kml\/',$html);
 	$tempB = explode('"]',$tempA[1]);
+	print_r($tempA);
 	return "http://www.mapalerter.ie/maie/kml/" . $tempB[0];
 }
 
