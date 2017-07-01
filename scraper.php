b2828f23bad82ac1dd41ef6ded5a6c275634e1db
John Handelaar <john@handelaar.org>
2017-07-01 18:38:40 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -47,46 +47,44 @@ foreach ($targets as $target) {
         $status = trim(html_entity_decode(str_replace('Status: ','',$statuspath->plaintext)),ENT_QUOTES);
         echo $status . "\n";
         
-        if (stristr('Decision Made Invalid Application Closed',$status)) {
-            # exit this loop if old/unwanted application
-            break; 
-        }
-        $receivedpath = $blobparser->find('ul',0)->find('li',6);
-        #echo $receivedpath->plaintext . "\n";
-        $date_received = date($date_format,strtotime(trim(html_entity_decode(str_replace('Application Received: ','',$receivedpath->plaintext)),ENT_QUOTES)));
-        $date_scraped = date($date_format);
-        $on_notice_from = $date_received;
-        $on_notice_to = date($date_format,(strtotime($on_notice_from) + 3024000)); # 35 days
-        $addresspath = $blobparser->find('ul',0)->find('li',2);
-        $address = trim(html_entity_decode(str_replace('Development Address: ','',$addresspath->plaintext)),ENT_QUOTES);
-        $descriptionpath = $blobparser->find('ul',0)->find('li',3);
-        $description = trim(html_entity_decode(str_replace('Development Description: ','',$descriptionpath->plaintext)),ENT_QUOTES);
-        
-        # http://maps.corkcoco.ie/planningenquirylitev3/Default.aspx?FullFileNumber=18a-175461&FromList=true
-        $info_url = 'http://maps.corkcoco.ie/planningenquirylitev3/Default.aspx?FullFileNumber=18a-' . str_replace('/','',str_replace('/0','/',$council_reference)) . '&FromList=true';
-        
-        $application = array(
-                'council_reference' => $council_reference,
-                'address' => $address,
-                'lat' => $lat,
-                'lng' => $lng,
-                'description' => $description,
-                'info_url' => $info_url,
-                'comment_url' => $council_comment_url,
-                'date_scraped' => $date_scraped,
-                'date_received' => $date_received,
-                'on_notice_from' => $on_notice_from,
-                'on_notice_to' => $on_notice_to
-            );
-        print_r($application);
-        die();
-        $existingRecords = scraperwiki::select("* from data where `council_reference`='" . $application['council_reference'] . "'");
-        if (sizeof($existingRecords) == 0) {
-            # print_r ($application);
-            scraperwiki::save(array('council_reference'), $application);
-        } else {
-            print ("Skipping already saved record " . $application['council_reference'] . "\n");
-        }
+        if (!(stristr('Decision Made Invalid Application Closed',$status))) {
+			$receivedpath = $blobparser->find('ul',0)->find('li',6);
+			#echo $receivedpath->plaintext . "\n";
+			$date_received = date($date_format,strtotime(trim(html_entity_decode(str_replace('Application Received: ','',$receivedpath->plaintext)),ENT_QUOTES)));
+			$date_scraped = date($date_format);
+			$on_notice_from = $date_received;
+			$on_notice_to = date($date_format,(strtotime($on_notice_from) + 3024000)); # 35 days
+			$addresspath = $blobparser->find('ul',0)->find('li',2);
+			$address = trim(html_entity_decode(str_replace('Development Address: ','',$addresspath->plaintext)),ENT_QUOTES);
+			$descriptionpath = $blobparser->find('ul',0)->find('li',3);
+			$description = trim(html_entity_decode(str_replace('Development Description: ','',$descriptionpath->plaintext)),ENT_QUOTES);
+			
+			# http://maps.corkcoco.ie/planningenquirylitev3/Default.aspx?FullFileNumber=18a-175461&FromList=true
+			$info_url = 'http://maps.corkcoco.ie/planningenquirylitev3/Default.aspx?FullFileNumber=18a-' . str_replace('/','',str_replace('/0','/',$council_reference)) . '&FromList=true';
+			
+			$application = array(
+					'council_reference' => $council_reference,
+					'address' => $address,
+					'lat' => $lat,
+					'lng' => $lng,
+					'description' => $description,
+					'info_url' => $info_url,
+					'comment_url' => $council_comment_url,
+					'date_scraped' => $date_scraped,
+					'date_received' => $date_received,
+					'on_notice_from' => $on_notice_from,
+					'on_notice_to' => $on_notice_to
+				);
+			print_r($application);
+			die();
+			$existingRecords = scraperwiki::select("* from data where `council_reference`='" . $application['council_reference'] . "'");
+			if (sizeof($existingRecords) == 0) {
+				# print_r ($application);
+				scraperwiki::save(array('council_reference'), $application);
+			} else {
+				print ("Skipping already saved record " . $application['council_reference'] . "\n");
+			}
+		}
     }    
 }
 
