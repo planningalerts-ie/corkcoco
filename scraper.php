e53f86d29deb44ec5c81fb27c03d519d545f1a98
John Handelaar <john@handelaar.org>
2017-07-01 18:24:04 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -28,63 +28,63 @@ unset($dom,$html);
 // Collect KML embedded in those URLs
 $kmls = array();
 foreach ($targets as $target) {
-	$fetch = file_get_contents($target);
-	$kml =  simplexml_load_file(getKML($fetch));
+    $fetch = file_get_contents($target);
+    $kml =  simplexml_load_file(getKML($fetch));
 
-	foreach ($kml->Document->Folder->Placemark as $item) {
-		$council_reference = str_replace("File Ref: ","",$item->name);
-		echo "Found $council_reference\n";
+    foreach ($kml->Document->Folder->Placemark as $item) {
+        $council_reference = str_replace("File Ref: ","",$item->name);
+        echo "Found $council_reference\n";
 
-		$point = explode(',',$item->Point->coordinates);
-		$lat = $point[1];
-		$lng = $point[0];
-		
-		$blob = (string)$item->description;
-		$blobparser = new simple_html_don();
-		$blobparser->load($blob);
-		
-		$statuspath = $parsethis->find('ul',0)->find('li',5);
-		$status = trim(html_entity_decode(str_replace('<B>Status: </B>'.'',$statuspath->plaintext)),ENT_QUOTES);
-		if stristr('Decision Made Invalid Application Closed',$status) {
-			# exit this loop if old/unwanted application
-			break; 
-		}
-		$receivedpath = $parsethis->find('ul',0)->find('li',6);
-		$date_received = date($date_format,strtotime(trim(html_entity_decode(str_replace('<B>Application Received: </B>'.'',$receivedpath->plaintext)),ENT_QUOTES)));
-		$date_scraped = date($date_format);
-		$on_notice_from = $date_received;
-		$on_notice_to = date($date_format,(strtotime($on_notice_from) + 3024000)); # 35 days
-		$addresspath = $parsethis->find('ul',0)->find('li',2);
-		$address = trim(html_entity_decode(str_replace('<B>Development Address: </B>'.'',$addresspath->plaintext)),ENT_QUOTES);
-		$descriptionpath = $parsethis->find('ul',0)->find('li',3);
-		$description = trim(html_entity_decode(str_replace('<B>Development Description: </B>'.'',$descriptionpath->plaintext)),ENT_QUOTES);
-		
-		# http://maps.corkcoco.ie/planningenquirylitev3/Default.aspx?FullFileNumber=18a-175461&FromList=true
-		$info_url = 'http://maps.corkcoco.ie/planningenquirylitev3/Default.aspx?FullFileNumber=18a-' . str_replace('/','',str_replace('/0','/',$council_reference)) . '&FromList=true';
-		
-		$application = array(
-				'council_reference' => $council_reference,
-				'address' => $address,
-				'lat' => $lat,
-				'lng' => $lng,
-				'description' => $description,
-				'info_url' => $info_url,
-				'comment_url' => $council_comment_url,
-				'date_scraped' => $date_scraped,
-				'date_received' => $date_received,
-				'on_notice_from' => $on_notice_from,
-				'on_notice_to' => $on_notice_to
-			);
-	    print_r($application);
-	    die();
-		$existingRecords = scraperwiki::select("* from data where `council_reference`='" . $application['council_reference'] . "'");
-		if (sizeof($existingRecords) == 0) {
-			# print_r ($application);
-			scraperwiki::save(array('council_reference'), $application);
-		} else {
-			print ("Skipping already saved record " . $application['council_reference'] . "\n");
-		}
-	}	
+        $point = explode(',',$item->Point->coordinates);
+        $lat = $point[1];
+        $lng = $point[0];
+        
+        $blob = (string)$item->description;
+        $blobparser = new simple_html_don();
+        $blobparser->load($blob);
+        
+        $statuspath = $parsethis->find('ul',0)->find('li',5);
+        $status = trim(html_entity_decode(str_replace('<B>Status: </B>'.'',$statuspath->plaintext)),ENT_QUOTES);
+        if (stristr('Decision Made Invalid Application Closed',$status)) {
+            # exit this loop if old/unwanted application
+            break; 
+        }
+        $receivedpath = $parsethis->find('ul',0)->find('li',6);
+        $date_received = date($date_format,strtotime(trim(html_entity_decode(str_replace('<B>Application Received: </B>'.'',$receivedpath->plaintext)),ENT_QUOTES)));
+        $date_scraped = date($date_format);
+        $on_notice_from = $date_received;
+        $on_notice_to = date($date_format,(strtotime($on_notice_from) + 3024000)); # 35 days
+        $addresspath = $parsethis->find('ul',0)->find('li',2);
+        $address = trim(html_entity_decode(str_replace('<B>Development Address: </B>'.'',$addresspath->plaintext)),ENT_QUOTES);
+        $descriptionpath = $parsethis->find('ul',0)->find('li',3);
+        $description = trim(html_entity_decode(str_replace('<B>Development Description: </B>'.'',$descriptionpath->plaintext)),ENT_QUOTES);
+        
+        # http://maps.corkcoco.ie/planningenquirylitev3/Default.aspx?FullFileNumber=18a-175461&FromList=true
+        $info_url = 'http://maps.corkcoco.ie/planningenquirylitev3/Default.aspx?FullFileNumber=18a-' . str_replace('/','',str_replace('/0','/',$council_reference)) . '&FromList=true';
+        
+        $application = array(
+                'council_reference' => $council_reference,
+                'address' => $address,
+                'lat' => $lat,
+                'lng' => $lng,
+                'description' => $description,
+                'info_url' => $info_url,
+                'comment_url' => $council_comment_url,
+                'date_scraped' => $date_scraped,
+                'date_received' => $date_received,
+                'on_notice_from' => $on_notice_from,
+                'on_notice_to' => $on_notice_to
+            );
+        print_r($application);
+        die();
+        $existingRecords = scraperwiki::select("* from data where `council_reference`='" . $application['council_reference'] . "'");
+        if (sizeof($existingRecords) == 0) {
+            # print_r ($application);
+            scraperwiki::save(array('council_reference'), $application);
+        } else {
+            print ("Skipping already saved record " . $application['council_reference'] . "\n");
+        }
+    }    
 }
 
 echo "....done.\n";
@@ -92,10 +92,10 @@ echo "....done.\n";
 
 
 function getKML($html) {
-	$tempA = explode("mapalerter.ie\/maie\/kml\/",$html);
-	$tempB = explode('"]',$tempA[1]);
-	#print_r($tempA);
-	return 'http://www.mapalerter.ie/maie/kml/' . $tempB[0];
+    $tempA = explode("mapalerter.ie\/maie\/kml\/",$html);
+    $tempB = explode('"]',$tempA[1]);
+    #print_r($tempA);
+    return 'http://www.mapalerter.ie/maie/kml/' . $tempB[0];
 }
 
 // // Write out to the sqlite database using scraperwiki library
